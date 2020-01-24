188f25ce5da48d87ea3f7b46ee119f422f0cf548
indrarahul2013 <indrarahul2013@gmail.com>
2020-01-24 16:29:02 +0530

    update

---
+++ lib/login.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'register.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      routes: <String, WidgetBuilder>{
+        '/register': (BuildContext context) => new RegisterPage()
+      },
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                  child: Text(
+                    'Cazz',
+                    style: GoogleFonts.lato( textStyle: TextStyle(
+                      fontSize: 80.0, fontWeight: FontWeight.bold
+                    )
+                  )),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(155.0, 115.0, 0.0, 0.0),
+                  child: Text(
+                    '.',
+                    style: TextStyle(
+                      fontSize: 80.0, fontWeight: FontWeight.bold, color: Colors.green
+                    )
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top:35.0, left:20.0,right:20.0),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'REG ID',
+                    labelStyle: GoogleFonts.lato(textStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey
+                    )),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green)
+                    )
+                  ),
+                ),
+                SizedBox(height: 35.0,),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'PASSWORD',
+                    labelStyle:  GoogleFonts.lato(textStyle:TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey
+                    )),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green)
+                    )
+                  ),
+                  obscureText: true,
+                ),
+                  SizedBox(height: 20.0,),
+                Container(
+                  alignment: Alignment(1.0,0.0),
+                  padding: EdgeInsets.only(top: 15.0, left: 20.0),
+                  child: InkWell(
+                    child: Text(
+                      'Forgot Password',
+                      style:  GoogleFonts.montserrat( textStyle: TextStyle(
+                        color: Colors.pink ,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline 
+                        )),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 100,),
+          Container(
+            height: 60.0,
+            color: Colors.transparent,
+            child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    style: BorderStyle.solid,
+                    width: 1.0
+                  ),
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(30.0)
+                ),
+                child: 
+                    Center(
+                      child: Text(
+                        'LOGIN',
+                        style: GoogleFonts.montserrat(textStyle: TextStyle(
+                          fontWeight: FontWeight.bold
+                        ))
+                      )
+                      )
+            ),
+          )
+              ],
+            ),
+          ),
+          SizedBox(height: 40,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'New to Cazz ?',
+                style: GoogleFonts.montserrat(textStyle: TextStyle(
+                    fontWeight: FontWeight.w600
+                ))
+              ),
+              SizedBox(width: 5.0,),
+              InkWell(
+                onTap: () {
+                  Navigator.of(context).pushNamed('/register');
+                },
+                child: Text(
+                  'Register',
+                  style: GoogleFonts.montserrat(textStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.pinkAccent,
+                    decoration: TextDecoration.underline
+                  ))
+                ),
+              )
+            ],
+          )         
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/register.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                  child: Text(
+                    'Cazzaa',
+                    style: GoogleFonts.lato( textStyle: TextStyle(
+                      fontSize: 80.0, fontWeight: FontWeight.bold
+                    )
+                  )),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(155.0, 115.0, 0.0, 0.0),
+                  child: Text(
+                    '.',
+                    style: TextStyle(
+                      fontSize: 80.0, fontWeight: FontWeight.bold, color: Colors.green
+                    )
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top:35.0, left:20.0,right:20.0),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'EMAIL',
+                    labelStyle: GoogleFonts.lato(textStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey
+                    )),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green)
+                    )
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'PASSWORD',
+                    labelStyle:  GoogleFonts.lato(textStyle:TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey
+                    )),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green)
+                    )
+                  ),
+                  obscureText: true,
+                ),
+                  SizedBox(height: 20.0,),
+                Container(
+                  alignment: Alignment(1.0,0.0),
+                  padding: EdgeInsets.only(top: 15.0, left: 20.0),
+                  child: InkWell(
+                    child: Text(
+                      'Forgot Password',
+                      style:  GoogleFonts.montserrat( textStyle: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline 
+                        )),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 100,),
+          Container(
+            height: 60.0,
+            child: Material(
+                borderRadius: BorderRadius.circular(30.0),
+                color: Colors.green,
+                child: GestureDetector(
+                  onTap:() {},
+                  child: Center(
+                    child: Text(
+                      'LOGIN',
+                      style: GoogleFonts.poppins(textStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                          fontWeight: FontWeight.w600
+                      ))
+                    ),
+                  ),
+                ),
+            ),
+          ),
+          SizedBox(height: 30,),
+          Container(
+            height: 60.0,
+            color: Colors.transparent,
+            child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    style: BorderStyle.solid,
+                    width: 1.0
+                  ),
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(30.0)
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Center(
+                      child: ImageIcon(
+                        AssetImage('assets/facebook.png')
+                      ),
+                    ),
+                    SizedBox(width: 10,),
+                    Center(
+                      child: Text(
+                        
+                        'Login with Facebook',
+                        style: GoogleFonts.montserrat(textStyle: TextStyle(
+                          fontWeight: FontWeight.bold
+                          
+                        ))
+                      )
+                      )
+                  ],
+                ),
+            ),
+          )
+              ],
+            ),
+          ),
+          SizedBox(height: 40,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'New to Cazz ?',
+                style: GoogleFonts.montserrat(textStyle: TextStyle(
+                    fontWeight: FontWeight.w600
+                ))
+              ),
+              SizedBox(width: 5.0,),
+              InkWell(
+                onTap: () {},
+                child: Text(
+                  'Register',
+                  style: GoogleFonts.montserrat(textStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green,
+                    decoration: TextDecoration.underline
+                  ))
+                ),
+              )
+            ],
+          )         
+        ],
+      ),
+    );
+  }
+}
